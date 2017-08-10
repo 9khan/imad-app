f0c8b105566217ba345433a8b00386e8536b537d
9khan <30683786+9khan@users.noreply.github.com>
2017-08-10 15:12:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,6 +24,22 @@ button.onclick = function ()
 request.open('GET','http://mohd9khan.imad.hasura-app.io/counter',true);
 request.send(null);
 };
+//submit names
+var nameInput = document.getElementById("name");
+var name = nameInput.value;
+var submit = document.getElementById("submit_btn");
+submit.onclick = function ()
+{
+    //make a request to the server and send the same
+    var names = ['name1','name2','name3','name4'];
+    var list = '';
+    for(var i=0;i<names.length;i++)
+    {
+        list += '<li>' + names[i] + '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+};
 /*console.log('Loaded!');
 //changing text
 var element = document.getElementById("main-text");
