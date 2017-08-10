be2b71ea28375f2f1c499d411166b07b13609d63
9khan <30683786+9khan@users.noreply.github.com>
2017-08-10 15:58:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ var articles =
         <p>
             This is the first article This is the first article This is the first article This is the first article This is the first article This is the first article This is the first article This is the first article 
         </p>`
+        `<input type = "text" name = "commentBox" value = "feedback"/>`
 },
    'article-two':
    {
