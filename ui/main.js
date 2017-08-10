20549c1de620ff2c16de9826f71ec80057b23b96
9khan <30683786+9khan@users.noreply.github.com>
2017-08-10 12:18:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,10 @@
 console.log('Loaded!');
+//changing text
 var element = document.getElementById("main-text");
 element.innerHTML = "THIS IS KING KHAN";
+//moving image
+var img = document.getElementById("king");
+img.onClick = function()
+{
+img.style.marginLeft = "100px";
+};
