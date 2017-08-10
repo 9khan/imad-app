bc9e3eb7dde6b640e569497c7045e00403d3b80d
9khan <30683786+9khan@users.noreply.github.com>
2017-08-10 15:21:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -112,6 +112,14 @@ app.get('/:articleName',function(req,res)
      res.send(createTemplate(articles[articleName]));
 }
 );
+var names = [];
+app.get('/submit-name/:name',function (req,res)
+{
+    var name = req.params.name;
+    names.push(name);
+    // json javascript object
+    res.send(JSON.stringify(names));
+})
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
