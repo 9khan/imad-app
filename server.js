170213f0a473e089fb66c1ccff464b949a387131
9khan <30683786+9khan@users.noreply.github.com>
2017-08-10 15:29:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -104,6 +104,14 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
+var names = [];
+app.get('/submit-name',function (req,res)
+{
+    var name = req.query.name;
+    names.push(name);
+    // json javascript object
+    res.send(JSON.stringify(names));
+});
 app.get('/:articleName',function(req,res)
 {
     //articleName = articel-one
@@ -112,14 +120,6 @@ app.get('/:articleName',function(req,res)
      res.send(createTemplate(articles[articleName]));
 }
 );
-var names = [];
-app.get('/submit-name',function (req,res)
-{
-    var name = req.query.name;
-    names.push(name);
-    // json javascript object
-    res.send(JSON.stringify(names));
-})
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
